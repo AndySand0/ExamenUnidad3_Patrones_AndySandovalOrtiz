1ea10b033d595de115328714753b3edfa4690370
AndySand0 <l21210547@tectijuana.edu.mx>
2025-11-17 23:29:54 -0800

    Create CategoriaDecorator.cs

---
+++ src/CategoriaDecorator.cs
@@ -0,0 +1,15 @@
+public class CategoriaDecorator : ProductoDecorator
+{
+    private readonly string _categoria;
+
+    public CategoriaDecorator(IProducto producto, string categoria)
+        : base(producto)
+    {
+        _categoria = categoria;
+    }
+
+    public override string ObtenerDescripcion()
+    {
+        return base.ObtenerDescripcion() + $" | Categoría: {_categoria}";
+    }
+}
